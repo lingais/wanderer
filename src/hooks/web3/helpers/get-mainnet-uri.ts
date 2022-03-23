e263afcf950efa1c31fd2b83b2314a09d5b206a4
lingais <95277184+lingais@users.noreply.github.com>
2022-03-24 00:59:41 +0800

    Update get-mainnet-uri.ts

--- src/hooks/web3/helpers/get-mainnet-uri.ts
+++ src/hooks/web3/helpers/get-mainnet-uri.ts
@@ -1,3 +1,6 @@
 export const getMainnetURI = (): string => {
     return "https://api.avax.network/ext/bc/C/rpc";
 };
+export const getTestnetURI = (): string => {
+  return "https://eth-rinkeby.alchemyapi.io/v2/aF5TH9E9RGZwaAUdUd90BNsrVkDDoeaO";
+};
